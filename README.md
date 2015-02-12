fee1122c972760a60777184a0caab1d544287b71
Joel Trost <joelt@zillow.com>
2015-02-12 15:43:53 -0800

    first

--- README.md
+++ README.md
@@ -1,2 +1,70 @@
-# promised-rest-client
+# Promised Rest Client
+
 A simple rest client I, @thecaddy, use to hit apis that return json
+
+### API
+
+
+```
+var data = require('promised-rest-client')(myUrl);
+
+//Overloaded Method uses
+data.get(obj)
+data.get(url, obj)
+
+//Remaining CRUD
+data.update(...)  //PUT 
+data.insert(...)  //POST 
+data.post(...)    //POST
+data.delete(...)  //DELETE
+```
+
+The object takes several parameters:
+
+```
+var obj = {
+  url: 'path/to/my/thing',
+  qs: {
+    myquery: 'string equals this'
+  },
+  body: {
+    mybody: {
+      hasmore: 'things',
+      awesome: true
+    }
+  }
+}
+
+```
+
+### Usage
+
+All queries return a promise.
+
+##### Example:
+
+
+```
+var data = require('promised-rest-client')('http://woohoo.com/')
+
+//Submits a get request to 'http://woohoo.com/bam/pow/shoot?count=true'
+data.get({
+  url: 'bam/pow/shoot',
+  qs:{
+    count: true
+  }
+}).then(function(resp){
+//Submits a get request to 'http://woohoo.com/bam/pow/shoot'
+//with body json { count: false }
+  return data.update('bam/pow/bang', {
+    body: {
+      count: false
+    }
+  })
+}).then(function(resp){
+  console.log(resp)
+}).catch(function(err){
+  console.error(err)
+})
+
+```
---
+++ index.js
@@ -0,0 +1,111 @@
+'use strict';
+
+var _ = require('lodash');
+var request = require('request');
+
+function getUrl(api, obj){
+  var url = api + obj.url;
+  return url;
+}
+
+function submit(options){
+  return new Promise(function(resolve, reject) {
+    // do a thing, possibly async, then…
+    request(options, function(err, resp, body){
+      if(err) reject(err);
+      else {
+        if(resp.statusCode === 200){
+          try{
+            resolve(JSON.parse(resp.body));
+          }catch(e){
+            reject(new Error(resp.statusCode + ': ' + resp.body));
+          }
+        }else if(resp.statusCode === 204){
+          resolve(resp.body);
+        }else{
+          reject(new Error(resp.statusCode + ': ' + resp.body));
+        }
+      }
+    })
+  })
+}
+
+//overloaded methods
+//get(obj)
+//get(url, obj)
+
+function getReqObj(url, obj){
+  if(typeof url === 'object') return url;
+  else {
+    obj.url = url
+    return obj
+  }
+}
+
+
+module.exports = function(api){
+  var obj;
+  return {
+    get: function(url, obj){
+      obj = getReqObj(url, obj);
+
+      var options ={
+        url: getUrl(api, obj),
+        method: 'GET'
+      }
+      if(obj.qs && !_.isEmpty(obj.qs)){
+        options.qs = obj.qs
+      }
+      return submit(options);
+    },
+    update: function(url, obj){
+      obj = getReqObj(url, obj)
+
+      var options ={
+        url: getUrl(api,obj),
+        method: 'PUT'
+      }
+      if(obj.qs && !_.isEmpty(obj.qs)){
+        options.qs = obj.qs
+      }
+      if(obj.body && !_.isEmpty(obj.body)){
+        options.form = obj.body
+      }
+
+      return submit(options);
+    },
+    insert: function(url, obj){
+      obj = getReqObj(url, obj)
+
+      var options ={
+        url: getUrl(api,obj),
+        method: 'POST'
+      }
+      if(obj.qs && !_.isEmpty(obj.qs)){
+        options.qs = obj.qs
+      }
+      if(obj.body && !_.isEmpty(obj.body)){
+        options.form = obj.body
+      }
+      return submit(options);
+    },
+    delete: function(url, obj){
+      obj = getReqObj(url, obj)
+
+      var options ={
+        url: getUrl(api,obj),
+        method: 'DELETE'
+      }
+      if(obj.qs && !_.isEmpty(obj.qs)){
+        options.qs = obj.qs
+      }
+      if(obj.body && !_.isEmpty(obj.body)){
+        options.form = obj.body
+      }
+      return submit(options);
+    },
+    post: function(url, obj){
+      return insert(url, obj)
+    }
+  }
+}
